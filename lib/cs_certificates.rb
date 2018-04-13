5132bb9c00fbdc2f71cfa492060d9b503623f930
Joel Tanner Jones <jonesjoe@ecu.edu>
2018-04-13 10:38:02 -0400

    Add gem root method

--- lib/cs_certificates.rb
+++ lib/cs_certificates.rb
@@ -2,3 +2,9 @@ require "cs_certificates/version"
 require "cs_certificates/qr_code"
 require "cs_certificates/certificate"
 require "cs_certificates/udl_module_certificate"
+
+module CsCertificates
+  def self.root
+    File.dirname __dir__
+  end
+end
